8836ddafa8f9172ac14da0239e267a9149a64efe
princelakragithub <princelakragithub.com>
2026-07-10 20:48:44 +0530

    1py

---
+++ lesson.py
@@ -0,0 +1,12 @@
+print("hello Python") #print()--in-built output funtion
+print(5+2)
+print("My Name Is Prince")
+print("Class - 10th")
+a=60
+print('add 3 to a variable that is 60;',a+10) 
+
+
+# activity-2 continue here
+print('All keywords pf python are displayed below;')
+help('keywords')
+help('modules')
