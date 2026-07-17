ec2a156ab4cfe2382829eb94ea524421a56aac86
princelakragithub <princelakragithub.com>
2026-07-17 20:40:21 +0530

    report

---
+++ wreport.py
@@ -0,0 +1,43 @@
+city= input("Enter the city name;")
+temp= float(input("Enter the temperature in C;"))
+
+print("City:", city)
+print("Temperature:", temp, "C")
+
+# Part-2
+if temp > 35:
+    print("It's a hot day in")
+
+
+#part-3
+
+    if temp < 25:
+        print("Great day to go outside!")
+    else:
+        print("Grab a jacket before you go outside!")
+
+
+#part-4
+
+if temp < 35:
+   print("Weather: scorching Hot")
+elif temp < 25:
+   print("Weather: Warm and Sunny")
+
+elif temp < 15:
+    
+  print("Weather: Cool and Breezy")
+
+else:
+
+ print("Weather: Cold - stay warm!")
+
+#part-5
+import datetime
+import calendar
+
+now = datetime.datetime.now()
+print("City:", city)
+print("Time now:", now)
+
+print(calendar.calendar(now.year))
