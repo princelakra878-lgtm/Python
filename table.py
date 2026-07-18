7e735dcad9b96370a2da7056718e077d98211049
princelakragithub <princelakragithub.com>
2026-07-18 21:08:25 +0530

    3

--- table.py
+++ table.py
@@ -1,2 +1,16 @@
 for i in range(1, 11):
     print(f"23 x {i} = {23 * i}")
+
+adj = ["red", "healthy", "tasty",]
+fruits = ["apple", "banana", "mango"]
+
+for x in adj:
+    for y in fruits:
+        print(x, y)
+
+n = int(input("Enter the number of rows: "))
+
+for i in range(1, n+1):
+    for j in range(i):
+        print("*", end="")
+    print()
