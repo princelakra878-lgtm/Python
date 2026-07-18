bd005ab99dabd311a7e209f9b828733cdcba64f9
princelakragithub <princelakragithub.com>
2026-07-18 21:22:23 +0530

    final

--- table.py
+++ table.py
@@ -25,3 +25,17 @@ while num <= 10:
     num += 1
 
 print(f"The sum of the first 10 natural is: {total_sum}")
+
+#All project
+
+num = int(input("Enter a number:"))
+
+if num > 1:
+    for i in range(2, int(num**0.5) + 1):
+        if (num % i) == 0:
+            print(f"{num} is not a prime number.")
+            break
+    else:
+        print(f"{num} is a prime number")
+else:
+    print(f"{num} is not a prime number.")
