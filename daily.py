8fea689978e7ec29b510ef938c85e39c8e51d2e1
princelakragithub <princelakragithub.com>
2026-07-24 18:39:06 +0530

    dailyadviser

---
+++ daily.py
@@ -0,0 +1,10 @@
+mood = input("Enter your mood: ").lower()
+
+if mood == "happy":
+    print("😊 Keep smiling!")
+elif mood == "sad":
+    print("💙 Better days are coming.")
+elif mood == "angry":
+    print("😌 Take a deep breath.")
+else:
+    print("🌞 Have a nice day!")
